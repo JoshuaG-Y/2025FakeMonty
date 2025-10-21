5488e05503764ad9e32fee84f5e191f9677002dc
JoshuaG-Y <hcps-groveryja@henricostudents.org>
2025-10-21 08:36:56 -0400

    start drivetrain

---
+++ src/main/java/frc/robot/subsystems/drive/DrivetrainIO.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems.drive;
+
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Pose2d;
+
+public interface DrivetrainIO {
+    @AutoLog
+    public static class DrivetrainIOInputs{
+        public Pose2d pose = new Pose2d();
+        public double leftOutputV = 0.0;
+        public double rightOutputV = 0.0;
+
+        // Both in meters per second
+        public double leftVelocity = 0.0;
+        public double rightVelocity = 0.0;
+
+        // Both in meters
+        public double leftPosM = 0.0;
+        public double rightPosM = 0.0;
+
+        public double[] leftCurrentAmps = new double[0];
+        public double[] rightCurrentAmps = new double[0];
+    }
+
+    // Function to update inputs for class above. This will be put in periodic() function
+    public void updateInputs(DrivetrainIOInputs inputs);
+    public void arcadeDrive(double left, double right);
+}
